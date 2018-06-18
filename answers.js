b16e73419c0d0d31098cd6a62781cbd487088ff6
Alexandre Dohrmann <alexandredohrmann@Alexandres-MacBook-Pro.local>
2018-06-18 17:08:41 -0600

    1st commit - selecting datatypes.

---
+++ answers.js
@@ -0,0 +1,73 @@
+// *****************************************************************************
+
+// Weekend Homework (w02d01):
+
+// *****************************************************************************
+
+// ---------------------
+// Answer the Following:
+// ---------------------
+
+	// For each of the following, write which datatypes you would use to represent the data, 
+	// and then give a small example of the data structure:
+
+	// 1.  A light switch that can be either on or off.
+
+		// Here ce would use a Boolean to check whether the light is on or off (true or false).
+			
+			// let light = "on";
+			// if (light = "on") {
+			// 	console.log(true);
+			// } else {
+			// 	console.log(false);
+			// }
+
+	// 2.  A user's email address.
+
+		// Here we could use a string "alexandrejdohrmann@gmail.com" to represent email address.
+
+			// let email = "me@me.com";
+
+	// 3.  A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+
+		// Here we could use an object "spaceship" with key values:
+		// "hull, laser blasters, tractors beam, and warp drive" as descriptors.
+
+			// const ship = {
+			//         lasers: 4,
+			//         meson guns: 18,
+			//         shields: true,
+			//     }
+
+	// 4.  A list of student names from our class.
+
+		// We could use strings and an array here to list out all the names as strings within the array.
+
+		// let students = [“johnny”, “mary”, “alf”, “frankie”];
+		  
+
+	// 5.  A list of student names from our class, each with a location.
+
+		// We could use an object along with key variables and arrays with strings:
+
+			// const students = {
+			// 	names: ["johnny", "mary", "alf", "frankie"],
+			// 	location: ["Littleton", "Denver", "SF", "NYC"],
+			// }
+
+	// 6.  A list of student names from our class, each with a location and each with a list of favorite tv shows.
+
+		// We could use an object along with key variables and arrays with strings:
+
+			const students = {
+				names: ["johnny", "mary", "alf", "frankie"],
+				location: ["Littleton", "Denver", "SF", "NYC"],
+				favoriteTvShows: ["The Walking Dead", "Barry", "Last Man On Earth"],
+			}
+
+// ---------------------
+// Answer the Following:
+// ---------------------		
+
+
+
